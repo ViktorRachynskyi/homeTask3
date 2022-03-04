0cdbb0d00096768b34b5944844366699a8c37518
Viktor <viktor.rachynskyi@asquad.io>
2022-03-04 18:38:24 +0200

    commit test

--- src/main/java/qa/guru/JavaTypes.java
+++ src/main/java/qa/guru/JavaTypes.java
@@ -1,4 +1,13 @@
 package qa.guru;
 
 public class JavaTypes {
+    public static void main(String[] args) {
+        short tShort;
+        byte tByte;
+        int tInt;
+        float tFloat;
+        double tDouble;
+        long tLong;
+        String tString;
+    }
 }
